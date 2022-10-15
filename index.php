8249cf3604e9a2af93201c3d1c8940decc7de712
Grouparray <alexjoshiyke@gmail.com>
2022-10-15 17:43:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
+    <title>PHP Registration Form</title>
     <link type="text/css" rel="stylesheet" href="reg_page.css">
     <script src="https://kit.fontawesome.com/f96eb84820.js" crossorigin="anonymous"></script>
 </head>
